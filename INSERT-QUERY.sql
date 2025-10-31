7488168a9d089cb205ba265d2613ccdc7cdf024b
alishbam76001 <alishbam76001@gmail.com>
2025-10-31 08:12:40 -0700

    final commit

---
+++ INSERT-QUERY.sql
@@ -0,0 +1,4 @@
+--INSERTING VALUES INTO BOOKS TABLE
+INSERT INTO books(isbn, book_title, category,rental_price,status, author, publisher)
+VALUES ('978-0-06-112005-8','UNKNOWN', 'CLASSIC',200.03,'YES','A.K','UNKNOWN');
+
